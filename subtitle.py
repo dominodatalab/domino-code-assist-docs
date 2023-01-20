283c02f99ca254dd47e5751d8db68168d3401f86
Andrew Collier <andrew@fathomdata.dev>
2023-01-20 18:39:46 +0000

    feat: Add title frames

--- subtitle.py
+++ subtitle.py
@@ -4,6 +4,7 @@ import re
 import os
 import os.path
 import sys
+from PIL import Image
 
 PADDING = 70
 FONTSIZE = 50
@@ -11,11 +12,12 @@ FPS = 5
 IMAGE_FORMAT = "png"
 
 MP4 = sys.argv[1]
-GIF = re.sub("mp4", "gif", MP4)
 SUB = re.sub("mp4", "sub", MP4)
 
 MP4_FOLDER = os.path.dirname(MP4)
 FRAMES_FOLDER = os.path.join(MP4_FOLDER, "frames")
+
+GIF = os.path.join(MP4_FOLDER, os.path.split(MP4_FOLDER)[-1] + ".gif")
 
 try:
     os.mkdir(FRAMES_FOLDER)
@@ -31,7 +33,7 @@ CMD = [
     "-loglevel error",
     f"-i {MP4}",
     f"-r {FPS}",
-    f"{FRAMES_FOLDER}/frame-%03d.{IMAGE_FORMAT}",
+    f"{FRAMES_FOLDER}/%05d-frame.{IMAGE_FORMAT}",
 ]
 
 CMD = " ".join(CMD)
@@ -39,7 +41,7 @@ CMD = " ".join(CMD)
 os.system(CMD)
 print("Done!")
 
-FILES = glob.glob(f"{FRAMES_FOLDER}/frame-" + ("[0-9]" * 3) + f".{IMAGE_FORMAT}")
+FILES = glob.glob(f"{FRAMES_FOLDER}/" + ("[0-9]" * 5) + f"-frame.{IMAGE_FORMAT}")
 FILES.sort()
 
 print(f"Created {len(FILES)} frames.")
@@ -51,8 +53,8 @@ subs = pysubs2.load(SUB, encoding="utf-8", fps=1)
 # Convert times (milliseconds) to frame number.
 #
 for line in subs:
-    line.end = int(line.end / 1000 + 1)
-    line.start = int(line.start / 1000 + 1)
+    line.end = int(line.end / 1000)
+    line.start = int(line.start / 1000)
 
 
 def subtitle(frame_number):
@@ -65,7 +67,7 @@ def subtitle(frame_number):
 
 
 def frame_number(filename):
-    match = re.search("frame-(.*).png$", filename, re.IGNORECASE)
+    match = re.search("([0-9]*)-frame.png$", filename, re.IGNORECASE)
     if match:
         return int(match.group(1))
     else:
@@ -104,17 +106,47 @@ for frame in FILES:
 #
 PADDED = PADDED + PADDED[-1:] * (FPS * 2)
 
+# FIND IMAGE SIZE -----------------------------------------------------------------------------------------------------
+
+widths, heights = set(), set()
+
+for padded in list(set(PADDED)):
+    img = Image.open(padded)
+    #
+    widths.add(img.width)
+    heights.add(img.height)
+
+assert len(widths) == 1
+assert len(heights) == 1
+
+PADDED_WIDTH = list(widths)[0]
+PADDED_HEIGHT = list(heights)[0]
+
+# CREATE COVER --------------------------------------------------------------------------------------------------------
+
+# convert -size 1920x1150 canvas:black label-background.png
+# convert label-background.png -fill white -font Arial -pointsize 120 -gravity center -annotate +0+0 'Importing Data from S3' -fill "#0072cd" -pointsize 48 -gravity southeast -annotate +32+16 'http://dominodatalab.com/' -pointsize 72 -annotate +32+64 "Domino Data Lab" label-title.png
+# convert label-title.png \( domino-logo.png -scale 22% \) -gravity southeast -geometry +32+80 -composite 00000-title.png
+
+TITLE = os.path.join(FRAMES_FOLDER, "00000-title.png")
+
 # CREATE GIF ----------------------------------------------------------------------------------------------------------
 
-CMD = [
-    "gifski",
-    "-q",
-    f"-o {GIF}",
-    f"--fps {FPS}",
-    "--width 1280",
-    "--quality 100",
-] + PADDED
+CMD = (
+    [
+        "gifski",
+        "-q",
+        f"-o {GIF}",
+        f"--fps {FPS}",
+        "--width 1280",
+        "--quality 100",
+    ]
+    + [TITLE] * 5
+    + PADDED
+)
 CMD = " ".join(CMD)
+
+print(CMD)
 
 os.system(CMD)
 
@@ -122,8 +154,8 @@ print(f"Output file: {GIF}")
 
 # CLEANUP -------------------------------------------------------------------------------------------------------------
 
-for padded in PADDED:
-    try:
-        os.remove(padded)
-    except FileNotFoundError:
-        pass
+# for padded in PADDED:
+#     try:
+#         os.remove(padded)
+#     except FileNotFoundError:
+#         pass

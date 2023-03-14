88ab857053aa9edf52fb25a0b7452e959ebc1dd6
Andrew Collier <andrew@fathomdata.dev>
2023-03-14 16:32:37 +0000

    Deployed cc17268 to latest] with MkDocs 1.4.2 and mike 1.1.2

---
+++ latest]/videos/quick-snippets/code.py
@@ -0,0 +1,12 @@
+# Script authorship information.
+#
+# Replace placeholder with relevant values.
+#
+__author__ = ""
+__copyright__ = ""
+__credits__ = ["Bill Berry", "Peter Buck", "Mike Mills"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Scott Litt"
+__email__ = "scott@get-litt.com"
+__status__ = "Production"
---
+++ latest]/videos/quick-visualisation/code-snippet.py
@@ -0,0 +1,4 @@
+_base_var_1.update_traces(marker = {
+    "size": 4,
+    "line": {"width": 2, "color": "black"}
+})
